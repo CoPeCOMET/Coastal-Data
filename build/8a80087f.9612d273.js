d322b414cb9c44495fc7ca05340f0406cde43468
Evan B. Goldstein <ebgoldstein@gmail.com>
2021-05-20 15:30:13 -0400

    revert

---
+++ build/8a80087f.9612d273.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{77:function(a){a.exports=JSON.parse('{"allTagsPath":"/Coastal-Data/blog/tags","slug":"urban-development","name":"urban development","count":2,"permalink":"/Coastal-Data/blog/tags/urban-development"}')}}]);
---
+++ build/d00c40ae.25c5218c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[34],{89:function(a){a.exports=JSON.parse('{"allTagsPath":"/Coastal-Data/blog/tags","slug":"data","name":"data","count":3,"permalink":"/Coastal-Data/blog/tags/data"}')}}]);
